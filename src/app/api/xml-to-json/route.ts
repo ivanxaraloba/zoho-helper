9c6682b30dc433a052ca0121d64d87640fc376ea
ivanxaraloba <ivanxara@loba.com>
2024-10-23 11:45:40 +0100

    fix: typescript

--- src/app/api/xml-to-json/route.ts
+++ src/app/api/xml-to-json/route.ts
@@ -4,13 +4,13 @@ import convert from "xml-js";
 export async function POST(req: NextRequest) {
   try {
     const xmlString = await req.text();
-    const converted = convert.xml2json(xmlString);
+    const converted = convert.xml2json(xmlString, { compact: true, spaces: 4 }); // You can specify options here
 
-    return NextResponse.json(converted, { status: 200 });
-  } catch (err: any) {
-    return NextResponse.json(
-      { error: err?.message || "Failed to process request" },
-      { status: 500 }
-    );
+    return NextResponse.json(JSON.parse(converted), { status: 200 }); // Parse the JSON string before returning
+  } catch (err: unknown) {
+    // Check if err is an instance of Error for safer type handling
+    const errorMessage =
+      err instanceof Error ? err.message : "Failed to process request";
+    return NextResponse.json({ error: errorMessage }, { status: 500 });
   }
 }
