1a89fba78bc249f6eb5f230bd89780f2dc92eb7c
IvanXara <ivanxara@loba.com>
2025-06-12 11:50:24 +0100

    dev: test removeTextKey 1

--- src/app/api/convert/xml-to-json/actions.ts
+++ src/app/api/convert/xml-to-json/actions.ts
@@ -1,7 +1,7 @@
 export const removeTextKey = (obj: any) => {
     if (typeof obj !== 'object' || obj === null) return obj;
 
-    if (obj.hasOwnProperty('_text')) return obj._text;
+    if (typeof obj === "object") return obj._text;
 
     for (const key in obj) {
         obj[key] = removeTextKey(obj[key]);
